a3f15e216d66b500866af8db0c4519371dace5ef
Alejandro Cabeza <alejandrocabeza400@gmail.com>
2025-08-17 23:44:21 -0400

    fix: establecer valor predeterminado para la configuración de locale en request.ts

--- app/src/i18n/request.ts
+++ app/src/i18n/request.ts
@@ -1,7 +1,7 @@
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async () => {
-  const locale = process.env.NEXT_PUBLIC_DEFAULT_LOCALE;
+  const locale = process.env.NEXT_PUBLIC_DEFAULT_LOCALE || "es";
 
   return {
     locale,
