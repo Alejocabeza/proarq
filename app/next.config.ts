16f975478cbde39601728ed8476fa8a0777a43dc
Alejandro Cabeza <alejandrocabeza400@gmail.com>
2025-08-18 00:31:53 -0400

    fix: eliminar carga de archivo .env en desarrollo en next.config.ts

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import { loadEnvFile } from "process";
 
-if (process.env.NODE_ENV === "development") {
-  loadEnvFile("../../.env");
-}
-
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   reactStrictMode: true,
